7b53f1869ac7147a801b2a3d9ed2a14d82984845
Edward West <edward_job@163.com>
2024-09-11 15:13:34 +0800

    feat: 添加 types/node 插件，添加 项目别名配置， vite.config.ts tsconfig.app.json 中

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    alias: {
+      'a': resolve(__dirname, 'src'),
+      'components': resolve(__dirname, 'src/components'),
+      'views': resolve(__dirname, 'src/views'),
+      'assets': resolve(__dirname, 'src/assets'),
+      'utils': resolve(__dirname, 'src/utils'),
+    }
+  }
 })
